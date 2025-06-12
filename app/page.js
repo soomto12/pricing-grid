a203635874a8f6365f519560246e0adabf9d0645
unknown <edokasomtochukwu@gmail.com>
2025-06-12 14:45:10 +0100

    done

--- app/page.js
+++ app/page.js
@@ -8,18 +8,87 @@ export default function GridPricing(){
 <div className=" text-center uppercase"> 
   basic
   <h2 className="mt-5 text-5xl text-center font-serif "> 100GB</h2>
+  <h3 className="flex justify-center text-xs"> $1.99/Month</h3>
   <div className="flex justify-center">
 
 <a href="#" className="mt-2 inline-block  border border-violet-600 px-10 py-3 my-6 rounded-lg hover:bg-purple-900 hover:border-violet-800  duration-500 ">
 purchase
 </a>
   </div>
-  <div className="border-t border-slate-700">
-  </div>
-  <div className="p-8 bg-slate-600 rounded-b-xl mb-3 mx-3"> </div>
+ 
+  
 </div>
       </div>
+       <div className="border-t border-slate-700">
+     
+  </div>
+  <div className="p-8 mx-3 rounded-b-xl bg-slate-800 mb-3 ">
+       <div className="flex flex-col space-y-2 ">
+    <h3  className="text-center font-mono"> 100 Gb of storage</h3>
+    <h3 className="text-center"> option to add member </h3>
+    <h3 className="text-center font-mono"> extra member benefits </h3>
+  </div>
+     </div>
+  
       </div>
+
+        <div className="bg-violet-700 rounded-xl text-white">
+      <div className="p-8 mx-3 mt-8 rounded-t-xl bg-slate-800 ">
+<div className=" text-center uppercase"> 
+  Standard
+  <h2 className="mt-5 text-5xl text-center font-serif "> 200GB</h2>
+  <h3 className="flex justify-center text-xs"> $2.99/Month</h3>
+  <div className="flex justify-center">
+
+<a href="#" className="mt-2 inline-block  border border-violet-600 px-10 py-3 my-6 rounded-lg hover:bg-purple-900 hover:border-violet-800  duration-500 ">
+purchase
+</a>
+  </div>
+ 
+  
+</div>
+      </div>
+       <div className="border-t border-slate-700">
+     
+  </div>
+  <div className="p-8 mx-3 rounded-b-xl bg-slate-800 mb-3 ">
+       <div className="flex flex-col space-y-2 ">
+    <h3  className="text-center font-mono"> 200 Gb of storage</h3>
+    <h3 className="text-center"> option to add member </h3>
+    <h3 className="text-center font-mono"> extra member benefits </h3>
+  </div>
+     </div>
+  
+      </div>
+        <div className="bg-slate-700 rounded-xl text-white">
+      <div className="p-8 mx-3 mt-8 rounded-t-xl bg-slate-800 ">
+<div className=" text-center uppercase"> 
+  premium
+  <h2 className="mt-5 text-5xl text-center font-serif "> 2TB</h2>
+  <h3 className="flex justify-center text-xs"> $3.99/Month</h3>
+  <div className="flex justify-center">
+
+<a href="#" className="mt-2 inline-block  border border-violet-600 px-10 py-3 my-6 rounded-lg hover:bg-purple-900 hover:border-violet-800  duration-500 ">
+purchase
+</a>
+  </div>
+ 
+  
+</div>
+      </div>
+       <div className="border-t border-slate-700">
+     
+  </div>
+  <div className="p-8 mx-3 rounded-b-xl bg-slate-800 mb-3 ">
+       <div className="flex flex-col space-y-2 ">
+    <h3  className="text-center font-mono"> 2 tb of storage</h3>
+    <h3 className="text-center"> option to add member </h3>
+    <h3 className="text-center font-mono"> extra member benefits </h3>
+  </div>
+     </div>
+  
+      </div>
+    
        </div>
       </div>
     </div>
